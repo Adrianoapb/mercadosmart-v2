3ae66fe8d6fc850ae5c13dbd1cce29d6bd26dabd
Adrianoapb <125501712+Adrianoapb@users.noreply.github.com>
2026-04-17 14:29:31 -0300

    Delete js directory

--- js/cloud.js
+++
@@ -1,8 +0,0 @@
-/* MercadoSmart Cloud bootstrap */
-window.addEventListener('online', () => {
-  try {
-    if (DB.getSettings().cloudAutoSync !== false) {
-      DB.CloudSync.syncSmart({ silent: true }).catch(() => {});
-    }
-  } catch (e) { console.debug('Falha não crítica ao iniciar experiência de nuvem', e); }
-});
